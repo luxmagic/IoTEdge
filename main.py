13ca714698744c63023fa25fec99e7453c2970b3
osht <clay.00@inbox.ru>
2025-05-19 18:36:21 +0300

    feat: test mqtt-publicher

--- main.py
+++ main.py
@@ -1,36 +1,26 @@
 import paho.mqtt.client as mqtt
-from fastapi import FastAPI
+import time
 
-app = FastAPI()
-
-# MQTT config
 MQTT_BROKER = "localhost"
 MQTT_PORT = 1883
-MQTT_TOPIC = "iot/data"
+MQTT_TOPIC = "temperature"
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to MQTT Broker!")
-    client.subscribe(MQTT_TOPIC)
 
 def on_message(client, userdata, msg):
     payload = msg.payload.decode()
     print(f"Received message: {payload} on topic {msg.topic}")
-    # Тут можно сохранять в базу, отправлять WebSocket и т.п.
 
 mqtt_client = mqtt.Client()
 mqtt_client.on_connect = on_connect
 mqtt_client.on_message = on_message
 mqtt_client.connect(MQTT_BROKER, MQTT_PORT, 60)
 
-@app.on_event("startup")
-def start_mqtt():
-    mqtt_client.loop_start()
-
-@app.on_event("shutdown")
-def stop_mqtt():
-    mqtt_client.loop_stop()
-    mqtt_client.disconnect()
-
-@app.get("/")
-def read_root():
-    return {"message": "MQTT listener active"}
+while mqtt_client.is_connected:
+    for i in range(10):
+        payload = f"{i*2}°C"
+        mqtt_client.publish(MQTT_TOPIC, payload)
+        print(f"[PUBLISH] {payload}")
+        time.sleep(2)
+    mqtt_client.loop()
